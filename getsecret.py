3e1ef2ece9bb8f21baa46c2bf050d3763d4eb8a6
Thibault Deraymaeker <thibault.deraymaeker@gmail.com>
2023-09-15 07:09:42 +0000

    Added getsecret.py

--- getsecret.py
+++ getsecret.py
@@ -1,9 +1,41 @@
-import botocore 
-import botocore.session 
-from aws_secretsmanager_caching import SecretCache, SecretCacheConfig 
+# Use this code snippet in your app.
+# If you need more information about configurations
+# or implementing the sample code, visit the AWS docs:
+# https://aws.amazon.com/developer/language/python/
 
-client = botocore.session.get_session().create_client('secretsmanager')
-cache_config = SecretCacheConfig()
-cache = SecretCache( config = cache_config, client = client)
+import boto3
+from botocore.exceptions import ClientError
 
-secret = cache.get_secret_string('mysecret')
+
+def get_secret():
+
+    secret_name = "snowflake/capstone/login"
+    region_name = "eu-west-1"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name,
+        #aws_access_key_id="AKIAU5YMQWRQ4TGY2LGK",
+        #aws_secret_access_key="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"
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        # For a list of exceptions thrown, see
+        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        raise e
+
+    # Decrypts secret using the associated KMS key.
+    secret = get_secret_value_response['SecretString']
+
+    # Your code goes here.
+
+    print(secret)
+
+if __name__ == "__main__":
+    get_secret()
